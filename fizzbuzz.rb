ddeaf1683138cd540ad731c255c50ca189f3c840
Ferris Boran <ferris.boran@gmail.com>
2019-03-07 20:43:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  case int
+  when int % 3 == 0
+    return "Fizz"
+  when int % 5 == 0
+    return "Buzz"
+  when int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
